e46ccea789562ec81cb05c5abbae9372d608e4ba
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2023-08-24 13:22:06 +0300

    first commit

---
+++ js/news-api.js
@@ -0,0 +1,39 @@
+let rootElment = document.getElementById('root');
+
+// rootElment.innerHTML = "<p>Hello form js</p>"
+
+async function getApiData()
+{
+    let respons = await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3cbf93ca943744a2bc449cbeae047c6b');
+    let data = await respons.json();
+    return data.articles;
+}
+
+function newsHtmlCollection(object) {
+    let htmlCollection = ``;
+    for (const iterator of object) {
+
+        htmlCollection += `
+        <div class="news-card">
+                    <div class="news-card-image">
+                        <img src="${iterator.urlToImage}" class="width-100" alt="${iterator.title}">
+                    </div>
+                    <div class="news-card-detials">
+                        <h2><a href='${iterator.url}' target='_blank'>${iterator.title}</a></h2>
+                        <h6>${iterator.publishedAt}</h6>
+                        <p>
+                            ${iterator.description ?? 'Not Avaliable'}
+                        </p>
+                    </div>
+                </div>
+        `;
+    }
+
+    rootElment.innerHTML = htmlCollection;
+}
+
+(async function(){
+    let arrayOfNews  = await getApiData();
+    
+   newsHtmlCollection(arrayOfNews);
+})();
